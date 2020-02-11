523db55a554467d3756c894fe8da6121a043f075
Fezekile <fezekile101@gmail.com>
2020-02-11 14:14:52 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,10 @@
-
+provider "github" {
+  token        = "9e2b8460a5f4cc86613705836e4b1bc63bba3c5b"
+  organization = "BI-GitHub-Management-Cape-Town
+/
+"
+}
+resource "github_repository" "BI-Cape-Town-GitHub" {
+  name        = "BI-Cape-Town-GitHub"
+  description = "My new repository for use with Terraform"
+}
