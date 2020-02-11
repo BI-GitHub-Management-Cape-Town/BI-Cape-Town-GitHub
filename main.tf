4228afc2fcde19bf9219ac6b652595e2b9b56ea2
Fezekile <fezekile101@gmail.com>
2020-02-11 14:42:42 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,6 @@
 provider "github" {
-  token        = "9e2b8460a5f4cc86613705836e4b1bc63bba3c5b"
-  organization = "BI-GitHub-Management-Cape-Town
-/
-"
+  token = "9e2b8460a5f4cc86613705836e4b1bc63bba3c5b"
+  organization = "BI-GitHub-Management-Cape-Town"
 }
 resource "github_repository" "BI-Cape-Town-GitHub" {
   name        = "BI-Cape-Town-GitHub"
